9ef179e318765118ddb82996905d1904cc50320e
Mirtahir <Mirtahir@DESKTOP-U7HSUEE>
2026-09-23 00:31:38 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,93 @@
+﻿// 1ci tapsiriq
+//Console.WriteLine("Adinizi yazin");
+//string ad = Console.ReadLine();
+//Console.WriteLine("Yasinizi yazin");
+//int yas=int.Parse(Console.ReadLine());
+//Console.WriteLine("Boyunuzu yazin");
+//double boy =double.Parse(Console.ReadLine());
+//Console.WriteLine("Salam, menim adım " + ad + ". Menim " + yas +
+//                          " yasim var, boyum " + boy + " m-dir.");
+////2ci tapsiriq
+//const double PI = 3.14159;
+//Console.WriteLine("Radiusu daxil edin");
+//double r=double.Parse(Console.ReadLine());
+//double s= PI * r * r;
+//Console.WriteLine("Dairenin sahesi: " + s);
+
+// 3-cu tapsiriq
+//const double USD = 1.7;
+//const double EUR = 1.82;
+//Console.WriteLine("Meblegi daxil edin");
+//double manat=double.Parse(Console.ReadLine());
+//double dollar = manat / USD;
+//double avro = manat / EUR;
+//Console.WriteLine(dollar);
+//Console.WriteLine(avro);
+
+//4-cu tapsiriq
+//Console.Write("1-ci  qiymeti daxil et : ");
+//string qiymet1 = Console.ReadLine();
+
+//Console.Write("2-ci qiymeti daxil et: ");
+//string qiymet2 = Console.ReadLine();
+
+//Console.Write("3-cü  qiymeti daxil et: ");
+//string qiymet3 = Console.ReadLine();
+
+//Console.Write("4-cü  qiymeti daxil et: ");
+//string qiymet4 = Console.ReadLine();
+
+//Console.Write("5-ci  qiymeti daxil et: ");
+//string qiymet5 = Console.ReadLine();
+
+//int bal1 = int.Parse(qiymet1);
+//int bal2 = int.Parse(qiymet2);
+//int bal3 = int.Parse(qiymet3);
+//int bal4 = int.Parse(qiymet4);
+//int bal5 = int.Parse(qiymet5);
+
+//double orta = (bal1 + bal2 + bal3 + bal4 + bal5) / 5;
+//Console.WriteLine("Orta bal: " + orta);
+//if (orta < 51)
+//{
+//    Console.WriteLine("Kesildiniz");
+//}
+//else if (orta <= 90)
+//{
+//    Console.WriteLine("Orta nətice");
+//}
+//else
+//{
+//    Console.WriteLine("Ela netice");
+//}
+
+// 5-ci sual
+//const double faiz = 0.12;
+//Console.Write("İlkin meblegi daxil edin: ");
+//double ilkinMebleg = double.Parse(Console.ReadLine());
+
+//Console.Write("Nece il saxlayacaginizi daxil edin: ");
+//int ilSayi = int.Parse(Console.ReadLine());
+
+//double gelecekMebleg = ilkinMebleg * (1 + faiz * ilSayi);
+
+//Console.WriteLine("Gelecek mebleğ: " + gelecekMebleg);
+
+//6ci sual
+//Console.Write("Mesafeni  daxil edin: ");
+//double mesafe = double.Parse(Console.ReadLine());
+
+//Console.Write("Serf olunan yanacagi daxil edin: ");
+//double yanacaq = double.Parse(Console.ReadLine());
+
+//if (mesafe < 0 || yanacaq < 0)
+//{
+//    Console.WriteLine("Daxil edilen melumat yanlisdir!");
+//}
+//else
+//{
+//    double serfiyyat = (yanacaq / mesafe) * 100;
+
+//    Console.WriteLine("100 km wcwn serfiyat: " + serfiyyat + " litr");
+//}
+
